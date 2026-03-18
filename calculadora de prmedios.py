d5184a1f951247a037cd4cf599fac874099c9f4c
Isaac Daniel Vasquez Martinez <maztric@gmail.com>
2026-03-17 20:11:14 -0600

    Update calculadora de prmedios.py

--- calculadora de prmedios.py
+++ calculadora de prmedios.py
@@ -2,24 +2,25 @@ print ("sistema para calcular tu promedio")
 
 nombre = input("vamos a comenzar, dime tu primer nombre.")
 
-cali1= int(input (f"a ver {nombre}, cual es tu calificacion en matematicas? "))
-cali2 =int(input (f"a ver {nombre}, cual es tu calificacion en biologia? "))
-cali3 =int(input (f"a ver {nombre}, cual es tu calificacion en historia? "))
-cali4 =int(input (f"a ver {nombre}, cual es tu calificacion en inglés? "))
-cali5 =int(input (f"a ver {nombre}, cual es tu calificacion en robótica? "))
+materias = ["matemáticas", "biología", "historia", "inglés", "robótica"]
+calificaciones = []
 
-promedio = (cali1 + cali2 + cali3 + cali4+ cali5)  / 5
-if  promedio < 6:
-    print (f"pasaste con {promedio: .1f} {nombre}, reprovaste! ")
-elif  6 <= promedio < 7:
-    print (f"pasaste con {promedio: .1f} {nombre}, pasaste de panzaso! ")
-elif 7 <= promedio <8:
-    print (f"pasaste con {promedio: .1f} {nombre}, Nada mal! pero puede mejorar! ")
-elif 8<= promedio <9:
-    print (f"pasaste con {promedio: .1f} {nombre}, Muy buen trabajo! ")
-elif 9<= promedio <10:
-    print (f"pasaste con {promedio: .1f} {nombre}, Excelente trabajo, felicidades!")
-elif promedio == 10:
-    print (f"pasaste con {promedio: .1f} {nombre}, Eso fue magnifico! Muchas felicidades! ")
+for materia in materias:
+    cali = float(input(f"a ver {nombre}, ¿cuál es tu calificación en {materia}? "))
+    calificaciones.append(cali)
+
+promedio = sum(calificaciones) / len(calificaciones)
+if promedio < 6:
+    mensaje = "reprobaste!"
+elif promedio < 7:
+    mensaje = "pasaste de panzazo!"
+elif promedio < 8:
+    mensaje = "nada mal, pero puede mejorar!"
+elif promedio < 9:
+    mensaje = "muy buen trabajo!"
+elif promedio < 10:
+    mensaje = "excelente trabajo, felicidades!"
 else:
-    print ("comando invalido ")
+    mensaje = "eso fue magnífico! muchas felicidades!"
+
+print(f"pasaste con {promedio:.1f} {nombre}, {mensaje}")
